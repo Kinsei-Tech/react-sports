330fac0c3cfada4e5872171ab249765b1fac7eb5
Laís Martins <laismartins@MacBook-Air-de-Lais.local>
2022-08-31 10:51:58 -0300

    fix: components header and search input

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,47 @@
+import { InputContainer, StyledHeader, UserImage } from "./styles";
+import Logo from "../../Images/LogoWithBall.svg";
+import ProfilePicture from "../../Images/example-profile-picture.svg";
+import { AiFillCaretDown } from "react-icons/ai";
+import { SearchInput } from "../SearchInput";
+import { ModalHeader } from "../ModalHeader";
+import { useEffect, useRef, useState } from "react";
+
+export const Header = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const modalRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    function handleCloseModal(event: { target: any }) {
+      if (!modalRef.current?.contains(event.target)) {
+        setIsVisible(false);
+      }
+    }
+    document.addEventListener("mousedown", handleCloseModal);
+
+    return () => {
+      document.removeEventListener("mousedown", handleCloseModal);
+    };
+  }, [setIsVisible]);
+
+  return (
+    <StyledHeader>
+      <div className="simple-header">
+        <img src={Logo} alt="brand logo" />
+        <section>
+          <UserImage>
+            <img src={ProfilePicture} alt="user profile img" />
+          </UserImage>
+          <button onClick={() => setIsVisible(true)}>
+            <div className="icon">
+              <AiFillCaretDown />
+            </div>
+          </button>
+          {isVisible && <ModalHeader />}
+        </section>
+      </div>
+      <InputContainer>
+        <SearchInput />
+      </InputContainer>
+    </StyledHeader>
+  );
+};
