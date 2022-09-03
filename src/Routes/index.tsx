1dcbaf7876b0052da82442e2fb521cbd7814dd15
Wigo Rossim da Silva <wigorossim@hotmail.com>
2022-09-03 17:19:46 -0300

    feat: Added route to profile page

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "../Pages/Dashboard";
-import LoginPage from "../Pages/LoginPage";
-import RegisterPage from "../Pages/RegistrationPage";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from '../Pages/Dashboard';
+import LoginPage from '../Pages/LoginPage';
+import ProfilePage from '../Pages/ProfilePage';
+import RegisterPage from '../Pages/RegistrationPage';
 
 export const RoutesMain = () => (
   <Routes>
-    <Route path="/" element={<LoginPage />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/register" element={<RegisterPage />} />
-    <Route path="*" element={<Navigate replace to="/" />} />
+    <Route path='/' element={<LoginPage />} />
+    <Route path='/dashboard' element={<Dashboard />} />
+    <Route path='/register' element={<RegisterPage />} />
+    <Route path='/profile' element={<ProfilePage />} />
+    <Route path='*' element={<Navigate replace to='/' />} />
   </Routes>
 );
