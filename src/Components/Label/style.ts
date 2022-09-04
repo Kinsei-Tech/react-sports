26c3a924ff60c298d9ad17b78df172d46c92184f
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-09-04 11:54:07 -0300

    Fix-Refactor: components style

--- src/Components/Label/style.ts
+++ src/Components/Label/style.ts
@@ -1,8 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 const LabelStyled = styled.label`
   display: flex;
   text-align: start;
   flex-direction: column;
+  width: 100%;
   span {
     margin-left: 15px;
   }
--- src/Components/Select/style.ts
+++ src/Components/Select/style.ts
@@ -1,12 +1,12 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 const SelectStyle = styled.select`
   height: 30px;
   width: auto;
   padding-left: 10px;
   border: 2px solid var(--color-green-primary-hover);
   border-radius: 8px;
-
   background-color: var(--gray-0);
+  color: var(--color-green-primary-hover);
 
   &:focus {
     border-color: var(--color-green-primary-hover);
@@ -14,10 +14,6 @@ const SelectStyle = styled.select`
 
   &:hover {
     border: 2px solid var(--color-green-primary-hover);
-  }
-
-  &::placeholder {
-    color: var(--color-green-primary-hover);
   }
 `;
 export default SelectStyle;
