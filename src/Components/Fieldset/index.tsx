8127e9d0fb1d77e247a22f78905419ff9fbe60e8
Laís Martins <laismartins@MacBook-Air-de-Lais.local>
2022-09-06 05:57:18 -0300

    fix: fieldset text input

--- src/Components/Fieldset/index.tsx
+++ src/Components/Fieldset/index.tsx
@@ -1,6 +1,6 @@
+import { ReactNode } from 'react';
 import { FieldValues, UseFormRegister } from 'react-hook-form';
 import { v4 as uuid } from 'uuid';
-import Input from '../Input';
 import { FieldsetStyle } from './style';
 
 interface ICheckBox {
@@ -9,17 +9,11 @@ interface ICheckBox {
   value: string;
 }
 
-interface ITextInput {
-  placeholder: string;
-  name: string;
-  label: string;
-}
-
 interface IFieldSet {
   type: 'textInput' | 'checkbox';
   legend: string;
   checkboxArr?: ICheckBox[];
-  textInputArr?: ITextInput[];
+  children?: ReactNode;
   register: UseFormRegister<FieldValues>;
 }
 
@@ -27,49 +21,41 @@ const FieldSet = ({
   type,
   legend,
   checkboxArr,
-  textInputArr,
+  children,
   register,
 }: IFieldSet) => {
   const conditionalRender = () => {
     switch (type) {
       case 'checkbox':
         return (
-          <FieldsetStyle>
-            <legend>{legend}</legend>
-            <ul>
-              {checkboxArr?.map((option) => {
-                return (
-                  <li key={uuid()}>
-                    <label>
-                      <input
-                        type='checkbox'
-                        value={option.value}
-                        {...register(option.name)}
-                      />
-                      <span>{option.label}</span>
-                    </label>
-                  </li>
-                );
-              })}
-            </ul>
-          </FieldsetStyle>
+          <>
+            <FieldsetStyle>
+              <legend>{legend}</legend>
+              <ul>
+                {checkboxArr?.map((option) => {
+                  return (
+                    <li key={uuid()}>
+                      <label>
+                        <input
+                          type='checkbox'
+                          value={option.value}
+                          {...register(option.name)}
+                        />
+                        <span>{option.label}</span>
+                      </label>
+                    </li>
+                  );
+                })}
+              </ul>
+            </FieldsetStyle>
+            {children}
+          </>
         );
       case 'textInput':
         return (
           <FieldsetStyle>
             <legend>{legend}</legend>
-            {textInputArr?.map((option) => {
-              return (
-                <Input
-                  key={uuid()}
-                  type='text'
-                  placeholder={option.placeholder}
-                  register={register}
-                  name={option.name}
-                  label={option.label}
-                />
-              );
-            })}
+            {children}
           </FieldsetStyle>
         );
     }
