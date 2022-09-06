b5616323a1a399639a83637fa9a99b62ddf7945c
Laís Martins <laismartins@MacBook-Air-de-Lais.local>
2022-09-06 08:48:03 -0300

    fix: getAddress function

--- src/Contexts/AddressContext.tsx
+++ src/Contexts/AddressContext.tsx
@@ -1,56 +1,53 @@
 import { createContext, Dispatch, SetStateAction, useState } from 'react';
-import { FieldValues } from 'react-hook-form';
 import { IProvider } from '../Interfaces/Global';
 import addressApi from '../services/addressApi';
 
 export const AddressContext = createContext<addressContextData>(
-   {} as addressContextData
+  {} as addressContextData
 );
 
-export interface addressContextData{
-    getAddress: (data: FieldValues) => void;
-    city: string | undefined;
-    setCity: Dispatch<SetStateAction<undefined>>;
-    state: string | undefined;
-    setState: Dispatch<SetStateAction<undefined>>;
+export interface addressContextData {
+  getAddress: (cep: string) => void;
+  city: string | undefined;
+  setCity: Dispatch<SetStateAction<undefined>>;
+  state: string | undefined;
+  setState: Dispatch<SetStateAction<undefined>>;
+  cep: string;
+  setCep: Dispatch<SetStateAction<string>>;
 }
 
-export interface addressResponse{
-    cep: string;
-	logradouro?: string;
-	complemento?: string;
-	bairro?: string;
-	localidade: string;
-	uf: string;
-	ibge?: string;
-	gia?: string;
-	ddd?: string;
-	siafi?: string;
+export interface addressResponse {
+  cep: string;
+  logradouro?: string;
+  complemento?: string;
+  bairro?: string;
+  localidade: string;
+  uf: string;
+  ibge?: string;
+  gia?: string;
+  ddd?: string;
+  siafi?: string;
 }
 
 const AddressProvider = ({ children }: IProvider) => {
+  const [city, setCity] = useState();
+  const [state, setState] = useState();
+  const [cep, setCep] = useState('');
 
-    const [city, setCity] = useState();
-    const [state, setState] = useState();
+  const getAddress = (cep: string) => {
+    addressApi.get(`${cep}/json/`).then((response) => {
+      setCity(response.data.localidade);
+      setState(response.data.uf);
+    });
+  };
 
-    const getAddress = (data: FieldValues) =>{
-        const getAddresApi = () =>{
-            const response = addressApi.get(`${data.cep}/json`, data).then((response) => {
-                setCity(response.data.localidade);
-                setState(response.data.uf);
-            })
-            return response;
-        }
-        getAddresApi();
-    }
+  return (
+    <AddressContext.Provider
+      value={{ getAddress, city, setCity, state, setState, cep, setCep }}
+    >
+      {children}
+    </AddressContext.Provider>
+  );
+};
 
-    return (
-        <AddressContext.Provider
-          value={{ getAddress, city, setCity, state, setState }}
-        >
-          {children}
-        </AddressContext.Provider>
-      );
-}
-
-export default AddressProvider
+export default AddressProvider;
