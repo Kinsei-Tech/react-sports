6ae8704592c0f395a36bf4349984618a43d4372f
Wigo Rossim da Silva <wigorossim@hotmail.com>
2022-09-06 14:13:20 -0300

    fix: Added changes between dashboard cards and profile cards

--- src/Components/TeamCard/index.tsx
+++ src/Components/TeamCard/index.tsx
@@ -1,8 +1,9 @@
 import '../../Pages/Dashboard/style';
-import { TeamCard } from './style';
+import { SectionImagem, TeamCard } from './style';
 import Button from '../../Components/Button';
 import { useState } from 'react';
 import DropRequestDashboard from '../DropRequestDashboard';
+import { FaEnvelope } from 'react-icons/fa';
 interface IElementsProps {
   name: string;
   placeName: string;
@@ -22,16 +23,27 @@ interface IElementsProps {
 interface ICard {
   // openModalTeamDetails?: () => void;
   elem: IElementsProps;
+  type?: string;
 }
-function Card({ elem }: ICard) {
+function Card({ elem, type }: ICard) {
   const [isVisible, setIsVisible] = useState(false);
   return (
     <TeamCard>
-      <img
-        src='https://cdn-icons-png.flaticon.com/128/1177/1177568.png'
-        alt='Logo do time'
-      />
+      <SectionImagem>
+        <figure>
+          <img
+            src='https://cdn-icons-png.flaticon.com/128/1177/1177568.png'
+            alt='Logo do time'
+          />
+        </figure>
 
+        {type === 'profile' && (
+          <div className='box-notification'>
+            <FaEnvelope className='svg-envelope-style' />
+            <span>2</span>
+          </div>
+        )}
+      </SectionImagem>
       <div>
         <h2>{elem.name}</h2>
         <p>{elem.description}</p>
@@ -46,29 +58,49 @@ function Card({ elem }: ICard) {
           {elem.positionsSearchedFor.join('/')}
         </span>
       </div>
-      <div className='btnContainer'>
-        <Button
-          color='#000000'
-          backGround='#93C335'
-          // onClick={openModalTeamDetails}
-        >
-          Detalhes
-        </Button>
-        <Button
-          onClick={() => setIsVisible(true)}
-          color='#000000'
-          backGround='#93C335'
-        >
-          Solicitar
-        </Button>
 
-        {isVisible && (
-          <DropRequestDashboard
-            isVisible={isVisible}
-            setIsVisible={setIsVisible}
-          />
-        )}
-      </div>
+      {type === 'profile' ? (
+        <div className='btnContainer'>
+          <Button
+            color='#000000'
+            backGround='#93C335'
+            // onClick={openModalTeamDetails}
+          >
+            Editar
+          </Button>
+          <Button
+            onClick={() => setIsVisible(true)}
+            color='#000000'
+            backGround='#93C335'
+          >
+            Excluir
+          </Button>
+        </div>
+      ) : (
+        <div className='btnContainer'>
+          <Button
+            color='#000000'
+            backGround='#93C335'
+            // onClick={openModalTeamDetails}
+          >
+            Detalhes
+          </Button>
+          <Button
+            onClick={() => setIsVisible(true)}
+            color='#000000'
+            backGround='#93C335'
+          >
+            Solicitar
+          </Button>
+
+          {isVisible && (
+            <DropRequestDashboard
+              isVisible={isVisible}
+              setIsVisible={setIsVisible}
+            />
+          )}
+        </div>
+      )}
     </TeamCard>
   );
 }
