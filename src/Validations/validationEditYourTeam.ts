9ede51500399c82c62fa50dad3a7a9d454bde3a9
Laís Martins <laismartins@MacBook-Air-de-Lais.local>
2022-09-06 08:50:21 -0300

    fix: cep with validation by length

--- src/Validations/validationEditYourTeam.ts
+++ src/Validations/validationEditYourTeam.ts
@@ -6,7 +6,7 @@ export const schemaEditYourTeam = yup.object().shape({
     .min(2, 'No minimo 2 caracteres')
     .required('Nome Obrigatorio'),
   placeName: yup.string(),
-  cep: yup.number(),
+  cep: yup.number().min(8, 'O CEP contém 8 dígitos, sendo todos numéricos'),
   state: yup.string().required('Campo obrigatorio'),
   city: yup.string().required('Campo Obrigatorio'),
   maxWeight: yup.number(),
