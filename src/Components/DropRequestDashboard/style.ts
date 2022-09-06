9433ce3abc31d52a593c48e4c659a0ef3f0a7625
Richard Zago <rich.czago@gmail.com>
2022-09-06 13:15:11 -0300

    refactor: changing class name and adjusting dropdown styling

--- src/Components/DropRequestDashboard/style.ts
+++ src/Components/DropRequestDashboard/style.ts
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.nav`
   background-color: var(--gray-0);
   border: 2px solid rgba(110, 148, 35, 0.72);
   border-radius: 8px;
   margin-left: 12px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 10px;
 
   div {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
   }
+
   button {
     background: transparent;
     border: none;
@@ -23,16 +28,29 @@ export const Container = styled.div`
   }
 
   @media (min-width: 930px) {
+    position: absolute;
+    top: 59px;
+    right: -90px;
+    flex-direction: column;
+    flex-wrap: nowrap;
     margin: 0px;
 
     button {
       margin: 0px;
     }
+
     svg {
       margin-right: 8px;
     }
-  }
 
-  @media (min-width: 1036px) {
+    .dropDown {
+      width: 110px;
+
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      align-items: flex-start;
+      padding: 0px;
+    }
   }
 `;
