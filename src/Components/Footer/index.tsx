cc5709b0ee0e82f7d2f3aab0f93dc4315389ec3c
Lucas do Couto Lemos <lemos120402@gmail.com>
2022-09-06 09:41:53 -0300

    feat: Footer component created

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { StyledFooter } from "./style";
+
+const Footer = () =>{
+    return(
+        <StyledFooter>
+            <span>
+                Copyright©, todos os direitos reservados
+            </span>
+        </StyledFooter>
+    )
+}
+
+export default Footer;
---
+++ src/Components/Footer/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+
+export const StyledFooter = styled.footer`
+    width: 100%;
+    height: 30px;
+    background-color: #93C335;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
