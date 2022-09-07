09bbe33d97c14c8eeb50f5bc59b44f3ce7e1ba1a
Wigo Rossim da Silva <wigorossim@hotmail.com>
2022-09-07 19:34:41 -0300

    feat: added function accRequestTeam to buttons

--- src/Components/Modal/ModalRequestTeam/CardRequestTeam/index.tsx
+++ src/Components/Modal/ModalRequestTeam/CardRequestTeam/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import TeamsProvider, { TeamsContext } from '../../../../Contexts/TeamsContext';
 import api from '../../../../services/api';
 import Button from '../../../Button';
 import { LiStyle } from './style';
@@ -16,16 +17,25 @@ const CardRequestTeam = ({
   userName,
   userPosition,
 }: ICardRequestTeamProps) => {
+  const { accRequestTeam } = useContext(TeamsContext);
   return (
     <>
       <p>
         {userName} solicitou entrada no time {teamsName} como {userPosition}.
       </p>
       <div>
-        <Button color='var(--gray-2)' backGround='var(--color-yellow-primary)'>
+        <Button
+          color='var(--gray-2)'
+          backGround='var(--color-yellow-primary)'
+          onClick={() => accRequestTeam(teamsName, idUserRequest, 'accepted')}
+        >
           Aceitar
         </Button>
-        <Button color='var(--gray-0)' backGround='var(--error)'>
+        <Button
+          color='var(--gray-0)'
+          backGround='var(--error)'
+          onClick={() => accRequestTeam(teamsName, idUserRequest, 'denied')}
+        >
           Recusar
         </Button>
       </div>
