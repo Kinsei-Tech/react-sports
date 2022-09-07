26738608ede4576ef837a60af8fb9797bd543281
Richard Zago <rich.czago@gmail.com>
2022-09-07 12:44:04 -0300

    refactor: adding new image input in html, adjusting size and name

--- src/Components/Modal/ModalCreateYourTeam/index.tsx
+++ src/Components/Modal/ModalCreateYourTeam/index.tsx
@@ -57,12 +57,19 @@ const ModalCreateYourTeam = () => {
           <article>
             <section>
               <Input
-                placeholder='Digite o nome do grupo'
+                placeholder='Digite o nome do time'
                 name='name'
                 register={register}
-                label='Nome do Grupo'
+                label='Nome do Time'
               />
               <ErrorMessage error={errors.name?.message} />
+
+              <Input
+                placeholder='Cole aqui a URL da imagem do time'
+                name='urlImg'
+                register={register}
+                label='Imagem do Time'
+              />
 
               <Input
                 name='placeName'
@@ -92,7 +99,7 @@ const ModalCreateYourTeam = () => {
                 placeholder='O que procura?'
                 register={register}
                 label='Descrição'
-                height='150px'
+                height='100px'
               />
               <ErrorMessage error={errors.description?.message} />
             </section>
