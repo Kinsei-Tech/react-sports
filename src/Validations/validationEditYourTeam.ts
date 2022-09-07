a235ad8491a81d3d8ca5719418603c7678b0aee6
Laís Martins <laismartins@MacBook-Air-de-Lais.local>
2022-09-07 19:07:59 -0300

    fix: required validations and feedback phases fixed

--- src/Validations/validationEditYourTeam.ts
+++ src/Validations/validationEditYourTeam.ts
@@ -6,11 +6,14 @@ export const schemaEditYourTeam = yup.object().shape({
     .min(2, 'No minimo 2 caracteres')
     .required('Nome Obrigatorio'),
   placeName: yup.string(),
-  cep: yup.number().min(8, 'O CEP contém 8 dígitos, sendo todos numéricos'),
-  state: yup.string().required('Campo obrigatorio'),
-  city: yup.string().required('Campo Obrigatorio'),
-  maxWeight: yup.number(),
-  maxAge: yup.number(),
+  cep: yup
+    .number()
+    .min(8, 'O CEP contém 8 dígitos, sendo todos numéricos')
+    .required(),
+  state: yup.string(),
+  city: yup.string(),
+  maxWeight: yup.string(),
+  maxAge: yup.string(),
   description: yup.string(),
   positionsSeachedfor: yup.array(),
 });
